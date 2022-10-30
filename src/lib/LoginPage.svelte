558d1a178e6d30115a99f75e918c30fef2809a74
MangoSwirl <collin.camerono@gmail.com>
2022-10-29 22:24:26 -0700

    Make sign in page and no permission page fullscreen on small screens

--- src/lib/LoginPage.svelte
+++ src/lib/LoginPage.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+  import CenteredBlockPage from "./reusable/CenteredBlockPage.svelte";
 
   function signIn() {
     const provider = new GoogleAuthProvider();
@@ -11,46 +12,14 @@
   }
 </script>
 
-<main>
-  <div class="center">
-    <div class="left">
-      <img src="src/assets/logo.jpg" alt="8033 logo">
-    </div>
-    <div class="right">
-      <h1>Sign in</h1>
-      <p>You need to sign in with an authorized account to access this tool.</p>
+<CenteredBlockPage>
+  <h1>Sign in</h1>
+  <p>You need to sign in with an authorized account to access this tool.</p>
 
-      <button data-variant="primary" on:click={signIn}>Sign in with Google</button>
-    </div>
-  </div>
-</main>
+  <button data-variant="primary" on:click={signIn}>Sign in with Google</button>
+</CenteredBlockPage>
 
 <style>
-  main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-
-    background-color: #520288;
-  }
-
-  .center {
-    background-color: #fff;
-    padding: 1rem;
-    border-radius: 0.4rem;
-
-    display: flex;
-    align-items: center;
-  }
-
-  .right {
-    margin: 1rem;
-  }
-
-  img {
-    height: 10rem;
-  }
 
   button {
     margin-top: 1rem;
