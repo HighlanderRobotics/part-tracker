c0f72c078b43ae3681ccb8d34334ca8958c234ca
MangoSwirl <collin.camerono@gmail.com>
2022-11-03 17:10:15 -0700

    Fix part sorting

--- src/logic/sortParts.ts
+++ src/logic/sortParts.ts
@@ -1,7 +1,11 @@
 import type { Part } from "src/types/Part";
+import estimateLifespan from "./estimateLifespan";
 const sortParts = (a: Part, b: Part) => {
+    const maxMatchesA = estimateLifespan(a);
+    const maxMatchesB = estimateLifespan(b);
+
     // @ts-ignore TypeScript doesn't like it when you subtract booleans because it sounds weird, but it was actually useful in this case.
     if (b.severeDamage !== a.severeDamage) return b.severeDamage-a.severeDamage;
-    return b.matchCount/b.maxMatches-a.matchCount/a.maxMatches;
+    return b.matchCount/maxMatchesB-a.matchCount/maxMatchesA;
 };
 export default sortParts;
